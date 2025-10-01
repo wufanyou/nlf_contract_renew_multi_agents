1183a6b875ecc3b845c5901ef3eccf0d9950d644
Fanyou Wu <fanyou.wu@outlook.com>
2025-09-30 22:13:37 -0700

    UPDATE

--- src/nfl_contract/crew.py
+++ src/nfl_contract/crew.py
@@ -1,149 +1,158 @@
-from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
-from crewai.agents.agent_builder.base_agent import BaseAgent
+import tempfile
 from typing import List
+import os
+import pandas as pd
+from crewai import Agent, Crew, Process, Task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai.knowledge.source.csv_knowledge_source import CSVKnowledgeSource
+from crewai.project import CrewBase, agent, crew, task
 
+CONTRACT_DATA = "./knowledge/contract_details.csv"
+STAT_DATA = "./knowledge/wr_stats_for_ai_negotiator.csv"
+PLAYER_ID = int(os.getenv("PLAYER_ID")) # type: ignore
 
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+def prepare_data(player_id: int) -> str:
+    df = pd.read_csv(CONTRACT_DATA)
+    df['contract_date'] = pd.to_datetime(df['contract_date'])
+    contract_date = df.iloc[player_id]["contract_date"]
+    df = df[df['contract_date'] < contract_date].reset_index(drop=True)
+    df = df.sort_values(by='contract_date', ascending=False).reset_index(drop=True)
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".csv")
+    df.to_csv(temp_file.name, index=False)
+    return temp_file.name
+
 
 @CrewBase
-class NflContract():
+class NflContract:
     """NflContract crew"""
 
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
     def nfl_player(self) -> Agent:
         return Agent(
-            config=self.agents_config['nfl_player'],  # type: ignore[index]
-            verbose=True
-        ) 
+            config=self.agents_config["nfl_player"], verbose=True  # type: ignore[index]
+        )
 
     @agent
     def player_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['player_agent'], # type: ignore[index]
-            verbose=True
+            config=self.agents_config["player_agent"],  # type: ignore[index]
+            verbose=True,
         )
 
     @agent
     def player_family(self) -> Agent:
         return Agent(
-            config=self.agents_config['player_family'], # type: ignore[index]
-            verbose=True
+            config=self.agents_config["player_family"],  # type: ignore[index]
+            verbose=True,
         )
 
     @agent
     def head_coach(self) -> Agent:
         return Agent(
-            config=self.agents_config['head_coach'], # type: ignore[index]
-            verbose=True
+            config=self.agents_config["head_coach"], verbose=True  # type: ignore[index]
         )
 
     @agent
     def team_owner(self) -> Agent:
         return Agent(
-            config=self.agents_config['team_owner'], # type: ignore[index]
-            verbose=True
+            config=self.agents_config["team_owner"], verbose=True  # type: ignore[index]
         )
 
     @agent
     def general_manager(self) -> Agent:
         return Agent(
-            config=self.agents_config['general_manager'], # type: ignore[index]
-            verbose=True
+            config=self.agents_config["general_manager"],  # type: ignore[index]
+            verbose=True,
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
     def player_performance_analysis(self) -> Task:
         return Task(
-            config=self.tasks_config['player_performance_analysis'], # type: ignore[index]
+            config=self.tasks_config["player_performance_analysis"],  # type: ignore[index]
         )
 
     @task
     def salary_cap_analysis(self) -> Task:
         return Task(
-            config=self.tasks_config['salary_cap_analysis'], # type: ignore[index]
+            config=self.tasks_config["salary_cap_analysis"],  # type: ignore[index]
         )
 
     @task
     def contract_terms_negotiation(self) -> Task:
         return Task(
-            config=self.tasks_config['contract_terms_negotiation'], # type: ignore[index]
+            config=self.tasks_config["contract_terms_negotiation"],  # type: ignore[index]
         )
 
     @task
     def family_financial_planning(self) -> Task:
         return Task(
-            config=self.tasks_config['family_financial_planning'], # type: ignore[index]
+            config=self.tasks_config["family_financial_planning"],  # type: ignore[index]
         )
 
     @task
     def roster_construction_analysis(self) -> Task:
         return Task(
-            config=self.tasks_config['roster_construction_analysis'], # type: ignore[index]
+            config=self.tasks_config["roster_construction_analysis"],  # type: ignore[index]
         )
 
     @task
     def coaching_evaluation(self) -> Task:
         return Task(
-            config=self.tasks_config['coaching_evaluation'], # type: ignore[index]
+            config=self.tasks_config["coaching_evaluation"],  # type: ignore[index]
         )
 
     @task
     def business_impact_analysis(self) -> Task:
         return Task(
-            config=self.tasks_config['business_impact_analysis'], # type: ignore[index]
+            config=self.tasks_config["business_impact_analysis"],  # type: ignore[index]
         )
 
     @task
     def risk_assessment_analysis(self) -> Task:
         return Task(
-            config=self.tasks_config['risk_assessment_analysis'], # type: ignore[index]
+            config=self.tasks_config["risk_assessment_analysis"],  # type: ignore[index]
         )
 
     @task
     def market_leverage_analysis(self) -> Task:
         return Task(
-            config=self.tasks_config['market_leverage_analysis'], # type: ignore[index]
+            config=self.tasks_config["market_leverage_analysis"],  # type: ignore[index]
         )
 
     @task
     def contract_recommendation(self) -> Task:
         return Task(
-            config=self.tasks_config['contract_recommendation'], # type: ignore[index]
-            output_file='contract_recommendation.md'
+            config=self.tasks_config["contract_recommendation"],  # type: ignore[index]
+            output_file=f"./output/{PLAYER_ID}/contract_recommendation.md",
         )
 
     @task
     def media_strategy_planning(self) -> Task:
         return Task(
-            config=self.tasks_config['media_strategy_planning'], # type: ignore[index]
-            output_file='media_strategy.md'
+            config=self.tasks_config["media_strategy_planning"],  # type: ignore[index]
+            output_file=f"./output/{PLAYER_ID}/media_strategy.md",
         )
 
     @crew
     def crew(self) -> Crew:
         """Creates the NflContract crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
+        knowledge_sources = [
+            CSVKnowledgeSource(
+                file_path=prepare_data(PLAYER_ID),
+                collection_name="other_players_contracts",
+            ),
+            CSVKnowledgeSource(
+                file_path=STAT_DATA, collection_name="wr_stats_for_recent_3_years"
+            ),
+        ]
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            knowledge_sources=knowledge_sources,  # type: ignore[call-arg]
         )
